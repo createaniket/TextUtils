a75289e0abce093587e7a60118d966c2dc0470a9
Aniket Pratap Singh <103597886+createaniket@users.noreply.github.com>
2022-11-07 01:15:34 +0530

    Updates

---
+++ precache-manifest.04146cc67ba0468c08ff0cffaf8b7e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf90dbbd4899725b1f42",
+    "url": "/TextUtils/static/css/main.f2b0b9cf.chunk.css"
+  },
+  {
+    "revision": "cf90dbbd4899725b1f42",
+    "url": "/TextUtils/static/js/main.cf90dbbd.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "68c03fd4f6f11a7591bb20ac52616762",
+    "url": "/TextUtils/index.html"
+  }
+];
